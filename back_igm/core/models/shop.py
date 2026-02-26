9359e47fef7725544d0d5cc1b9e2159472e9093b
juanse <juanseguzzardilopez@hotmail.com>
2026-02-26 00:19:13 -0300

    x

--- back_igm/core/models/shop.py
+++ back_igm/core/models/shop.py
@@ -1,5 +1,28 @@
+from product import Product
+from client import Client
 
+from typing import List, Dict, Any
 
 class Shop:
-    def __init__(self):
-        pass
+    def __init__(self,id:str, name:str, products:List[Product],clients:List[Client]):
+        self.id = id
+        self.name = name
+        self.products = products
+        self.clients = clients
+
+    @staticmethod
+    def fromJson(data: Dict[str, Any]) -> "Shop":
+        return Shop(
+            id=str(data["id"]),
+            name=str(data["name"]),
+            products=[Product.fromJson(p) for p in data.get("products", [])],
+            clients=[Client.fromJson(c) for c in data.get("clients", [])]
+        )
+
+    def toJson(self) -> Dict[str, Any]:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "products": [p.toJson() for p in self.products],
+            "clients": [c.toJson() for c in self.clients]
+        }
--- front_igm/CustomerView/models/Shop.js
+++ front_igm/CustomerView/models/Shop.js
@@ -1,14 +1,31 @@
 import { Product } from "./Product";
 import { Client } from "./Client";
 
-class Shop{
-    constructor(id,name,products,clients){
-        this.id = id;
-        this.name = name;
-        this.products = products;
-        this.clients = clients;
+export class Shop {
+    constructor(id, name, products = [], clients = []) {
+        this.id = id;            // string
+        this.name = name;        // string
+        this.products = products; // Array<Product>
+        this.clients = clients;   // Array<Client>
     }
 
-    static fromJson(json){}
-    toJson(){}
+    static fromJson(json) {
+        if (!json) return null;
+
+        return new Shop(
+            String(json.id),
+            String(json.name),
+            (json.products || []).map(p => Product.fromJson(p)),
+            (json.clients || []).map(c => Client.fromJson(c))
+        );
+    }
+
+    toJson() {
+        return {
+            id: this.id,
+            name: this.name,
+            products: this.products ? this.products.map(p => p.toJson()) : [],
+            clients: this.clients ? this.clients.map(c => c.toJson()) : []
+        };
+    }
 }
