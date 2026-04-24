d1821b85184b1e9a2e7fa7c3c670e5b3c36d34ee
juansemac <juanseguzzardilopez@hotmail.com>
2026-04-24 02:56:55 -0300

    sin miedo a borrar lo inutil

--- TestingConcepts/frontFW/api/index.js
+++
@@ -1,4 +0,0 @@
-export { AttributeApi } from "./attributeApi.js";
-export { CategoryApi }  from "./categoryApi.js";
-export { ProductApi }   from "./productApi.js";
-export { request, ApiError } from "./_request.js";
--- TestingConcepts/frontFW/interfaceModels/index.js
+++
@@ -1,5 +0,0 @@
-export { AttributeDTO }               from "./AttributeDTO.js";
-export { AttributeImplementationDTO } from "./AttributeImplementationDTO.js";
-export { VariantDTO }                 from "./VariantDTO.js";
-export { CategoryDTO }                from "./CategoryDTO.js";
-export { ProductDTO }                 from "./ProductDTO.js";
